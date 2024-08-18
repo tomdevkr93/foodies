4f1a4c2c033b55cf84a71ed688058a0cb475e6ec
Tom <tom.dev.kr@gmail.com>
2024-08-19 03:05:15 +0900

    prettier + eslint 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main>
-    <h1 style={{ color: 'white', textAlign: 'center' }}>
-      Time to get started!
-    </h1>
-  </main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
+    </main>
   );
 }
