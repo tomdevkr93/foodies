b99535d6736ac6c11a417703708f4849cdf3a491
Tom <tom.dev.kr@gmail.com>
2024-08-21 17:37:48 +0900

    MealForm 컴포넌트 분리 error 페이지 추가

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -1,6 +1,7 @@
 import ImagePicker from '@/src/components/meals/ImagePicker';
 import styles from './page.module.css';
 import { sharedMeal } from '../../lib/action';
+import MealForm from '@/src/components/meals/MealForm';
 
 export default function ShareMealPage() {
   return (
@@ -42,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
-            <button type="submit">Share Meal</button>
+            <MealForm />
           </p>
         </form>
       </main>
